f7397dfea1841ff67395b2006c3f4d8cb1d9aa1f
Josep Mulet Pol <jmuletpol@uoc.edu>
2024-01-19 14:43:21 +0100

    New build

--- version.php
+++ version.php
@@ -26,6 +26,6 @@ defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_codepro';
 $plugin->release = '1.0.2';
-$plugin->requires = 2023122200;
+$plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
 $plugin->version = 2024012001;
