8d554e6016239be2dd0804e32b61d0b54789cb4a
Josep Mulet Pol <jmuletpol@uoc.edu>
2025-12-18 12:57:45 +0100

    feat: Expand CI matrix to support Moodle 4.1-5.1 and add user preference storage.

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die();
 
 $plugin->component = 'tiny_codepro';
-$plugin->release = '2.1.3';
+$plugin->release = '2.1.4';
 $plugin->requires = 2022112800;
 $plugin->maturity = MATURITY_STABLE;
-$plugin->version = 2025103001;
+$plugin->version = 2025121801;
