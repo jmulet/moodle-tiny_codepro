724f7e6a88094e7706cf5ee2ca7aca30ede1e609
Josep Mulet Pol <jmuletpol@uoc.edu>
2025-12-19 17:55:18 +0100

    fix phpcs warnings

--- lib.php
+++ lib.php
@@ -21,6 +21,7 @@
  * @copyright   2025 Josep Mulet <pep.mulet@gmail.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
+// Keep MOODLE_INTERNAL check for backward compatibility with Moodle <4.3.
 // phpcs:ignore Moodle.Files.MoodleInternal.MoodleInternalNotNeeded
 defined('MOODLE_INTERNAL') || die();
 
