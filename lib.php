1e79acca0629b864f09afebaf2b3a93b1f5b6f7c
Josep Mulet Pol <jmuletpol@uoc.edu>
2025-12-19 17:42:53 +0100

    chore: Add phpcs ignore directive for Moodle.Files.MoodleInternal.MoodleInternalNotNeeded.

--- lib.php
+++ lib.php
@@ -21,7 +21,7 @@
  * @copyright   2025 Josep Mulet <pep.mulet@gmail.com>
  * @license     https://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
  */
-
+// phpcs:ignore Moodle.Files.MoodleInternal.MoodleInternalNotNeeded
 defined('MOODLE_INTERNAL') || die();
 
 /**
